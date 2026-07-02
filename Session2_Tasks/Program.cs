a3431b05c1e9f86d4fc13f9c3d2aedfd0abd7772
AL-WALEED AL-HILALI <wk3042711@gmail.com>
2026-07-02 13:04:08 +0400

    created branch for Session2_Tasks

---
+++ Session2_Tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session2_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
